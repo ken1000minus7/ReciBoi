921dcf5314e14120dc9423ee9c1f1ac9d25428aa
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-02-27 17:00:16 +0530

    user model created

--- app/src/main/java/com/miniweebs/reciboi/data/api/User.kt
+++ app/src/main/java/com/miniweebs/reciboi/data/api/User.kt
@@ -1,5 +1,22 @@
 package com.miniweebs.reciboi.data.api
 
 class User {
+    lateinit var name : String
+    lateinit var email : String
+    lateinit var image : String
     var mealsList : List<Meal> = mutableListOf()
+    constructor(name : String, email : String, image : String)
+    {
+        this.name=name
+        this.email=email
+        this.image=image
+    }
+
+    constructor(name : String, email : String, image : String, mealsList : List<Meal>)
+    {
+        this.name=name
+        this.email=email
+        this.image=image
+        this.mealsList=mealsList
+    }
 }
--- app/src/main/java/com/miniweebs/reciboi/presentation/MainActivity.kt
+++ app/src/main/java/com/miniweebs/reciboi/presentation/MainActivity.kt
@@ -2,16 +2,22 @@ package com.miniweebs.reciboi.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import com.miniweebs.reciboi.R
+import com.miniweebs.reciboi.data.api.User
 import com.miniweebs.reciboi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private var binding : ActivityMainBinding? = null
+    private lateinit var database : DatabaseReference
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,5 +28,25 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(setOf(R.id.randomMealFragment,R.id.searchFragment,R.id.userPrefFragment))
         setupActionBarWithNavController(navController,appBarConfiguration)
         bottomNavigationView.setupWithNavController(navController)
+        database = FirebaseDatabase.getInstance().reference
+        auth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        database.child("Users").child(auth.uid!!).addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                Log.d("check","check")
+                if(!p0.exists())
+                {
+                    Log.d("check",p0.toString())
+                    database.child("Users").child(auth.uid!!).setValue(User(auth.currentUser?.displayName!!,auth.currentUser?.email!!,auth.currentUser?.photoUrl.toString()!!))
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
