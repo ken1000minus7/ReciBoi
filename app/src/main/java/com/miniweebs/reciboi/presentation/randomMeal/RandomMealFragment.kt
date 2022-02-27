f86ba203da72192360d05a6660b44e1f7cfe965c
ken1000minus7 <manjotsinghoberoi.btcse20@pec.edu.in>
2022-02-27 15:13:34 +0530

    Yt video added

--- app/src/main/java/com/miniweebs/reciboi/presentation/randomMeal/RandomMealFragment.kt
+++ app/src/main/java/com/miniweebs/reciboi/presentation/randomMeal/RandomMealFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.miniweebs.reciboi.databinding.FragmentRandomMealBinding
@@ -29,9 +30,21 @@ class RandomMealFragment : Fragment() {
             binding.mealCategory.text = it.strCategory
             binding.tvInstructions.text = it.strInstructions
             val yt_link = it.strYoutube
-            binding.ytLink.setOnClickListener {
-                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(yt_link)))
+//            binding.ytLink.setOnClickListener {
+//                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(yt_link)))
+//            }
+            val videoView = binding.ytVideo
+            var done = false
+            var yt_id = ""
+            for(i in yt_link)
+            {
+                if(done) yt_id+=i
+                if(i=='=') done=true;
             }
+            videoView.settings.javaScriptEnabled=true
+            videoView.settings.layoutAlgorithm=WebSettings.LayoutAlgorithm.SINGLE_COLUMN
+            videoView.settings.pluginState=WebSettings.PluginState.ON
+            videoView.loadData("<html><body style=\" margin: 0px; padding: 0px;\"><iframe class=\"youtube-player\" type=\"text/html\" src=\"https://www.youtube.com/embed/$yt_id\" frameborder=\"0\" style=\"width : 100%; height:100%; margin:0px; padding:0px; border:0;\" allowfullscreen></iframe></body></html>","text/html","utf-8");
             Glide.with(requireContext()).load(it.strMealThumb).into(binding.mealImage)
         }
         return view
